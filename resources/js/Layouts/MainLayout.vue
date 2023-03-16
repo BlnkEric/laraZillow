2b32042ac77836d022efefb0aa30e941b39d6088
BlnkEric <amirsheybani.goyk1101@gmail.com>
2023-03-16 14:49:24 +0330

    main layout styles a littabit

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,26 @@
 <template>
-    <Link rel="stylesheet" href="/">Go to Home</Link>&nbsp;
-    <Link rel="stylesheet" href="/show">Go to show</Link>
-    <Link rel="stylesheet" href="/listings">Go to listings</Link>
-    <!-- <div>Timer is {{ timer }}</div> -->
-    <div v-if="successFlash" class="success">
-        {{ successFlash }}
-    </div>
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link rel="stylesheet" :href="route('listings.index')">Listings</Link>&nbsp;
+                </div>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link rel="stylesheet" :href="route('listings.index')">BlnkEric</Link>
+                </div>
+                <div>
+                    <Link rel="stylesheet" :href="route('listings.create')" class="bg-indigo-600 dark:bg-indigo-400 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">Add New Listing</Link>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main class="container mx-auto p-4">
+        <!-- <div>Timer is {{ timer }}</div> -->
+        <div v-if="successFlash" class="mb-4 border rounded-md shadow-sm border-green-300 dark:border-green-600 bg-green-100 dark:bg-green-900 p-4">
+            {{ successFlash }}
+        </div>
+    </main>
     <slot>Default</slot>
 </template>
 
@@ -28,10 +43,3 @@ const successFlash = computed(
 // const timer = ref(0)
 // setInterval(() => timer.value++, 1000)
 </script>
-
-<style scoped>
-.success {
-    background-color: green;
-    color: white;
-}
-</style>
