a7fa60220ee3dccaf72f627cbc61ac1fce0a9197
BlnkEric <amirsheybani.goyk1101@gmail.com>
2023-03-20 16:49:46 +0330

    watcher in realtor for delete filter

--- resources/js/Pages/Realtor/Index/Components/RealtorFilter.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilter.vue
@@ -1,10 +1,36 @@
 <template>
     <form action="">
-        <div class="my-4 flex flex-wrap gap-2">
+        <div class="my-5 flex flex-wrap gap-2">
             <div class="flex flex-nowrap items-center gap-2">
-                <input id="deleted" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                <input id="deleted" v-model="filterForm.deleted" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
                 <label for="deleted">Deleted</label>
             </div>
         </div>
     </form>
 </template>
+
+<script setup>
+import {reactive, watch} from 'vue'
+import {Inertia} from '@inertiajs/inertia'
+
+const filterForm = reactive({
+    deleted: false,
+})
+
+//for one singel property or watching one item changes
+watch(filterForm, () => 
+    // console.log(newValue, OldValue),
+    Inertia.get(
+        route('realtor.listings.index'),
+        filterForm,
+        {
+            preserveState: true,
+            preserveScroll: true,
+        },
+    ),
+)
+//if we got more than one thing we have to use an array
+// watch([() => filterForm.deleted, () => reactiveOrRefOrComputed.somethingElse], (newValue, OldValue) => 
+//     console.log(newValue, OldValue),
+// )
+</script>
